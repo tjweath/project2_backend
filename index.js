092701124cb5af42f405c50ed75b30fda426ce4d
tjweath <t_weatherseed@hotmail.com>
2024-01-15 20:06:06 +0000

    monday push

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.DATABASE_URL)
 
 const Schema = mongoose.Schema;
 
-const userSchema = new mongoose.Schema({
+const userSchema = new Schema({
     userEmail: {
         type: String,
         required: true
@@ -49,29 +49,104 @@ app.post('/user/login', async (req, res) => {
     }
     })
 
-    // const bookSchema = new Schema({
-    //     title: String,
-    //     author: {
-    //         type: mongoose.Schema.Types.ObjectId,
-    //         ref: 'Author'
-    //     }
-    // })
-
-    // const Book = mongoose.model('Book', bookSchema)
-
     const accountSchema = new Schema({
         name: String,
     })
 
-    const Account = mongoose.model('Account')
-
-    // Following app.get /book endpoint built in Library 
-    // app.get('/book', async (req, res) => {
-    //     const allBooks = await Book.find({}).populate('author')
-    //     res.json(allBooks)
-    // })
+    const activitySchema = new Schema({
+        activity: String,
+        day: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Day'
+        }
+    })
+    
+    const daySchema = new Schema({
+        day: String,
+    })
+    
+    const User = mongoose.model('User', userSchema)
+    const Account = mongoose.model('Account', accountSchema)
+    const Activity = mongoose.model('Activity', activitySchema)
+    const Day = mongoose.model('Day', daySchema)
 
     app.get('/account', async (req, res) => {
         const allAccount = await Account.find({}).populate('account')
         res.json(allAccount)
     })
+
+    app.post('/account/add', async (req, res) => {
+        if( await Activity.countDocuments({"activity": req.body.activity}) === 0 ) {
+            const newActivity = new Activity({activity: req.body.activity})
+            newActivity.save()
+            .then(() => {
+                addDay(req.body.activity)
+            })
+        }
+        else {
+            addDay(req.body.activity)
+        }
+    
+        async function addDay(reqActivity) {
+            const activity = await Activity.findOne({"activity": reqActivity})
+            const day = new Day({
+                day: req.body.day,
+                activity: activity,
+            })
+            day.save()
+            .then(() => {
+                console.log('New activity added')
+                res.sendStatus(200)
+            })
+            .catch(err => console.error(err))
+        }
+    })
+
+    app.get('/activity', async (req, res) => {
+        console.log('Received request for activity ID:', req.params.id);
+        try {
+            const allActivity = await Activity.find({}).populate('activity')
+            res.json(allActivity)
+        } catch (error) {
+            res.status(500).json({ error: error.message })
+        }
+    })
+
+    app.get('/activity/:id', async (req, res) => {
+        const activity = await Activity.findById(req.params.id).populate('day')
+        res.json(activity)
+    })
+
+
+
+
+
+    // app.put('/activity/:id', async (req, res) => {
+    //     try {
+    //       let day = await Day.findOne({ "day": req.body.day })
+    //       if (!day) {
+    //         const newDay = new Day({ name: req.body.day })
+    //         day = await newDay.save()
+    //       }
+      
+    //       const id = req.params.id
+    //       const updatedActivity = await Day.findByIdAndUpdate(
+    //         id,
+    //         {
+    //           activity: req.body.activity,
+    //           day: day._id
+    //         },
+    //         { new: true }
+    //       )
+      
+    //       if (!updatedActivity) {
+    //         return res.status(404).json({ message: "Activity not updated" })
+    //       }
+      
+    //       return res.status(200).json(updatedActivity)
+    //     } catch (err) {
+    //       console.log(err.message)
+    //       res.status(500).json({ message: 'Internal Server Error' })
+    //     }
+    //   })
+
